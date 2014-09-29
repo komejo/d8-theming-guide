10574e88b6613158be95e842073dd66cbd0d7814
Sander Tirez <sander.tirez@one-agency.be>
2014-09-29 12:55:50 +0200

    Javascript documentation code

--- src/awesome/js/awesome.js
+++ src/awesome/js/awesome.js
@@ -1,4 +1,7 @@
-(function($) {
-  $message = $('<p>Hello world</p>');
-  $('body').append($message);
-})(jQuery);
+(function ($) {
+  Drupal.behaviors.mymodule = {
+    attach: function (context, settings) {
+      $('main').append('<p>Hello world</p>');
+    }
+  };
+}(jQuery));
